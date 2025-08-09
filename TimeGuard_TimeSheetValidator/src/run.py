8ebc2f04aebbed4a21bb88276b51a2c1902f8a57
Moh123S <mohitsharma782000@gmail.com>
2025-08-09 10:59:52 +0530

    run

--- TimeGuard_TimeSheetValidator/src/run.py
+++ TimeGuard_TimeSheetValidator/src/run.py
@@ -1,5 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.app import create_app
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True, port=5000)
+         app = create_app()
+         app.run(debug=True, port=5000)
